3d2fcd474a7f4e067f2876d803e4d5ae2fddabfa
Chris <chris@chris-agnew.com>
2022-01-30 18:29:31 -0500

    add request page

---
+++ pages/request.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const Request = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const onChange = dates => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <DatePicker
+          selected={startDate}
+          onChange={onChange}
+          startDate={startDate}
+          endDate={endDate}
+          selectsRange
+          inline
+        />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Request;
