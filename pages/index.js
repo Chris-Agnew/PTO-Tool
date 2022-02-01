a7cc5835b462305482367bfc5e5650731f9cb661
chris-agnew <hello@chrisagnew.codes>
2022-02-01 17:00:31 -0500

    add firestore

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { signInWithGoogle, auth } from "../components/firebase/firebase";
-import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
-  console.group("Loading:", loading, "|", "Current User:", user);
+  // const [users, usersLoading, usersError] = useCollection(
+  //   firebase.firestore().collection("users")
+  // );
+  // if (!usersLoading && users) {
+  //   craftBlock.docs.map(doc => console.group(doc.data()));
+  // }
+
+  console.log("Loading:", loading, "|", "Current User:", user);
   return (
     <div>
       <Navbar user={user} />
