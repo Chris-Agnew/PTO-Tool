630299a417bbf5761f14a2671abcc4611c81993a
chris-agnew <hello@chrisagnew.codes>
2022-02-02 15:01:52 -0500

    add foot and nav to each page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../components/firebase/firebase";
 
 function MyApp({ Component, pageProps }) {
+  const [user, loading] = useAuthState(auth);
   return (
     <div className="font-nunito">
+      <Navbar user={user} />
       <Component {...pageProps} />
+      <Footer />
     </div>
   );
 }
