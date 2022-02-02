ea77fa9c71299372bd1ef0bdb8a4b09a978dc93c
chris-agnew <hello@chrisagnew.codes>
2022-02-02 15:02:12 -0500

    add google button

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { signInWithGoogle, auth } from "../components/firebase/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useCollection } from "react-firebase-hooks/firestore";
+import {
+  signInWithGoogle,
+  googleSignOut,
+} from "../components/firebase/firebase";
+import Image from "next/image";
+import GoogleButton from "react-google-button";
 
 export default function Home() {
-  const [user, loading, error] = useAuthState(auth);
-  // const [users, usersLoading, usersError] = useCollection(
-  //   firebase.firestore().collection("users")
-  // );
-  // if (!usersLoading && users) {
-  //   craftBlock.docs.map(doc => console.group(doc.data()));
-  // }
-
-  console.log("Loading:", loading, "|", "Current User:", user);
   return (
-    <div>
-      <Navbar user={user} />
-      <div>
-        <button onClick={signInWithGoogle}>Sign in</button>
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <Image
+          className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          alt="hero"
+          src="/images/arv-pto.jpg"
+          height={600}
+          width={600}
+        />
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            Welcome to the ARV PTO Tool
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Sign in Below to add your time off
+          </p>
+          <div className="flex justify-center">
+            <GoogleButton onClick={signInWithGoogle} />
+          </div>
+        </div>
       </div>
-      <Footer />
-    </div>
+    </section>
   );
 }
