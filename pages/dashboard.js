2a45865935dcc2bfd1cfd5a30f3198b434c7297a
chris-agnew <hello@chrisagnew.codes>
2022-02-07 15:16:29 -0500

    add auth, calendar, and dashboard info

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,34 +1,41 @@
+import { useAuth } from "../components/firebase/Auth";
+import DashboardInfo from "../components/dashboard/DashboardInfo";
+import Calendar from "../components/calendar";
+
 const dashboard = () => {
   return (
     <section className="text-gray-600 body-font">
+      <DashboardInfo />
+
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4 text-center">
           <div className="p-4 sm:w-1/4 w-1/2">
             <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
-              2.7K
+              2.7
             </h2>
-            <p className="leading-relaxed">Users</p>
+            <p className="leading-relaxed">PTO Days YTD</p>
           </div>
           <div className="p-4 sm:w-1/4 w-1/2">
             <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
-              1.8K
+              2.3
             </h2>
-            <p className="leading-relaxed">Subscribes</p>
-          </div>
-          <div className="p-4 sm:w-1/4 w-1/2">
-            <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
-              35
-            </h2>
-            <p className="leading-relaxed">Downloads</p>
+            <p className="leading-relaxed">PTO Days Left</p>
           </div>
           <div className="p-4 sm:w-1/4 w-1/2">
             <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
               4
             </h2>
-            <p className="leading-relaxed">Products</p>
+            <p className="leading-relaxed">Appointment Hours</p>
+          </div>
+          <div className="p-4 sm:w-1/4 w-1/2">
+            <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
+              4.5
+            </h2>
+            <p className="leading-relaxed">Other PTO days</p>
           </div>
         </div>
       </div>
+      <Calendar />
     </section>
   );
 };
