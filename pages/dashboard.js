73c2a0c536821e042ea09fcfb93c861872ea11d4
chris-agnew <hello@chrisagnew.codes>
2022-02-08 13:08:33 -0500

    add styling changes

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,9 +5,8 @@ import Calendar from "../components/calendar";
 const dashboard = () => {
   return (
     <section className="text-gray-600 body-font">
-      <DashboardInfo />
-
-      <div className="container px-5 py-24 mx-auto">
+      <div className="container px-5 py-24 mx-auto text-center">
+        <DashboardInfo />
         <div className="flex flex-wrap -m-4 text-center">
           <div className="p-4 sm:w-1/4 w-1/2">
             <h2 className="title-font font-medium sm:text-4xl text-3xl text-gray-900">
