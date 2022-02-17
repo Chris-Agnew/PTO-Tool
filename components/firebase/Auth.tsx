9e864a03a14ec341a58f9e6dbf43ad9c551bc6e8
Chris <chris@chris-agnew.com>
2022-02-16 21:22:43 -0500

    add user data type

--- components/firebase/Auth.tsx
+++ components/firebase/Auth.tsx
@@ -1,38 +1,38 @@
-import { createContext, useContext, useEffect, useState } from "react";
-const AuthContext = createContext({});
-import { getAuth } from "firebase/auth";
-import GoogleButton from "react-google-button";
-import { signInWithGoogle } from "./firebase";
-import Loading from "./Loading";
+import { createContext, useContext, useEffect, useState } from 'react'
+const AuthContext = createContext({})
+import { getAuth } from 'firebase/auth'
+import GoogleButton from 'react-google-button'
+import { signInWithGoogle } from './firebase'
+import Loading from './Loading'
 
 interface userData {
-  displayName: string;
-  email: string;
-  photoURL: string;
-  uid: string;
+  displayName: string
+  email: string
+  photoURL: string
+  uid: string
 }
 
-export const AuthProvider: React.FC<{}> = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState<any | null>(null);
-  const [loading, setLoading] = useState(true);
+export const AuthProvider: React.FC<userData> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<any | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const auth = getAuth();
-    return auth.onIdTokenChanged(async user => {
+    const auth = getAuth()
+    return auth.onIdTokenChanged(async (user) => {
       if (!user) {
-        console.log("no user");
-        setCurrentUser(null);
-        setLoading(false);
-        return;
+        console.log('no user')
+        setCurrentUser(null)
+        setLoading(false)
+        return
       }
-      const token = await user.getIdToken();
-      setCurrentUser(user);
-      setLoading(false);
-    });
-  }, []);
+      const token = await user.getIdToken()
+      setCurrentUser(user)
+      setLoading(false)
+    })
+  }, [])
 
   if (loading) {
-    return <Loading />;
+    return <Loading />
   }
 
   if (!currentUser) {
@@ -40,13 +40,13 @@ export const AuthProvider: React.FC<{}> = ({ children }) => {
       <div className="flex justify-center items-center">
         <GoogleButton onClick={signInWithGoogle} />
       </div>
-    );
+    )
   } else {
     return (
       <AuthContext.Provider value={{ currentUser }}>
         {children}
       </AuthContext.Provider>
-    );
+    )
   }
-};
-export const useAuth = () => useContext(AuthContext);
+}
+export const useAuth = () => useContext(AuthContext)
